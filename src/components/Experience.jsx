9624cf79749bc356e7ebea973c4b932c640fe810
Anton Chimrov <Anton.Chimrov@inp-e.com>
2023-04-18 13:35:27 +0200

    correct model components naming

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,20 +1,14 @@
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 //import GltfModel from "../utils/glbModel"
 
-
-
 const Experience = () => {
-
-    return (
-        <>
-            <OrbitControls />
-            <PerspectiveCamera makeDefault position={[60,50,60]} zoom={0.8}/>
-            <axesHelper args={[70]} />
-            <gridHelper args={[100, 10]} />
-            
-      
-
-        </>
-    )
-}
-export default Experience
+  return (
+    <>
+      <OrbitControls />
+      <PerspectiveCamera makeDefault position={[60, 50, 60]} zoom={0.8} />
+      <axesHelper args={[70]} />
+      <gridHelper args={[100, 10]} />
+    </>
+  );
+};
+export default Experience;
--- src/components/Light.jsx
+++ src/components/Light.jsx
@@ -1,12 +1,29 @@
 import { PointLight } from "three";
 
-export default function Light({ intensity, brightness1, brightness2 , color1, color2 }) {
-    return (
-        <>
-        <ambientLight intensity={intensity} />
-        <pointLight color={color1} intensity={brightness1} distance={90} decay={2} position={[10, 15, 10]} />
-        <pointLight color={color2} intensity={brightness2} distance={50} decay={2} position={[10, 45, 10]} />
-        
-        </>
-    )
+export default function Light({
+  intensity,
+  brightness1,
+  brightness2,
+  color1,
+  color2,
+}) {
+  return (
+    <>
+      <ambientLight intensity={intensity} />
+      <pointLight
+        color={color1}
+        intensity={brightness1}
+        distance={90}
+        decay={2}
+        position={[10, 15, 10]}
+      />
+      <pointLight
+        color={color2}
+        intensity={brightness2}
+        distance={50}
+        decay={2}
+        position={[10, 45, 10]}
+      />
+    </>
+  );
 }
