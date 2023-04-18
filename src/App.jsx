43d3aba9c1e47e5526766c96c5c0c33e5f58977c
Anton Chimrov <Anton.Chimrov@inp-e.com>
2023-04-18 14:45:58 +0200

    on click event

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { Canvas } from "@react-three/fiber"
-import Experience from "./components/Experience"
-import Light from "./components/Light"
-import Displays from "./components/Displays"
+import { Canvas } from "@react-three/fiber";
+import Experience from "./components/Experience";
+import Light from "./components/Light";
+import Displays from "./components/Displays";
 import React from "react";
-import  Model  from './components/ReservoirModel'
+import ReservoirModel from "./components/ReservoirModel";
 
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
-
   return (
-    <Canvas >
-      <Model />
-      <Light intensity = {0.3} brightness1={25} color1={"#707682"} brightness2={60} color2 = {"#6086b5"} />
-      <Displays />
+    <Canvas>
+      <ReservoirModel />
+      <Light
+        intensity={0.3}
+        brightness1={25}
+        color1={"#707682"}
+        brightness2={60}
+        color2={"#6086b5"}
+      />
+      
       <Experience />
     </Canvas>
-  )
+  );
 }
 
-export default App
+export default App;
