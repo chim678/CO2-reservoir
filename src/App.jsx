ad4c98dec015efb8185f1a5a619a5e42e01624dc
Anton Chimrov <Anton.Chimrov@inp-e.com>
2023-04-18 08:53:11 +0200

    modified model stucture

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import Experience from "./components/Experience"
 import Light from "./components/Light"
 import Displays from "./components/Displays"
 import React from "react";
-import ReservoirModel from './components/ReservoirModel'
+import  Model  from './components/ReservoirModel'
 
 import './App.css';
+
 
 
 function App() {
 
   return (
     <Canvas >
-      <ReservoirModel />
+      <Model />
       <Light intensity = {0.3} brightness1={25} color1={"#707682"} brightness2={60} color2 = {"#6086b5"} />
       <Displays />
       <Experience />
